2543d88fdfff5c3668a5cd841cd2d1881e3cde65
arefin_aareef <arefin.aareef@gmail.com>
2022-12-24 21:36:49 +0600

    defining API URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'; // importing hooks
 
 function App() {
-
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  
   return (
     <div className="App">
       
