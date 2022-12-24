19253e3a29bc9ec63270ebf9745dd43b2873af1c
arefin_aareef <arefin.aareef@gmail.com>
2022-12-24 22:09:09 +0600

    button props added

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Form = ({ reqType, setReqType }) => { // form is receiving properties --destructuring
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-        
+        <Button
+            buttonText="users" // button properties
+            reqType={reqType}
+            setReqType={setReqType}
+        />
+        <Button
+            buttonText="posts"
+            reqType={reqType}
+            setReqType={setReqType}
+        />
+        <Button
+            buttonText="comments"
+            reqType={reqType}
+            setReqType={setReqType}
+        />
     </form>
   )
 }
