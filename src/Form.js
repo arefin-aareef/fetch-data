8ab6e1de0a072dfd1ffb5720f7b1e12cc7e47616
arefin_aareef <arefin.aareef@gmail.com>
2022-12-24 22:03:43 +0600

    form component

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = ({ reqType, setReqType }) => { // form is receiving properties --destructuring
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+        
+    </form>
+  )
+}
+
+export default Form
