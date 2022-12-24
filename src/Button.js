07c451f551dad7d2a51ca38e9b9b2faf9bd4a2ad
arefin_aareef <arefin.aareef@gmail.com>
2022-12-24 23:14:17 +0600

    button component completed

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Button = ({ buttonText, reqType, setReqType }) => { // destructuring properties
   return (
-    <div>
-        
-    </div>
+    <button
+        className={buttonText === reqType ? "selected" : null}
+        type="button"
+        onClick={() => setReqType(buttonText)}
+    >
+        {buttonText}
+    </button>
   )
 }
 
