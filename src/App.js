ae5a877f60c12979048d6663f42462a2073119e2
arefin_aareef <arefin.aareef@gmail.com>
2022-12-24 21:53:22 +0600

    useEffect for fetching sources

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import { useState, useEffect } from 'react'; // importing hooks
 
 function App() {
   const API_URL = 'https://jsonplaceholder.typicode.com/';
-  
+  const [reqType, setReqType] = useState('users'); // putting req for data
+  const [items, setItems] = useState([]) // getting response array
+
+  useEffect(() => {
+    // useEffect will fetch different resources as they change
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json(); // got response, changed it to json
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchItems();
+
+  },[reqType]) // [] is dependencies
+
   return (
     <div className="App">
       
