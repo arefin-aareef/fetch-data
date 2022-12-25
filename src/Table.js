3aa3b48e7cc6f9649fcfcb90c6b28e61fc18c288
arefin_aareef <arefin.aareef@gmail.com>
2022-12-25 15:16:51 +0600

    table component added

---
+++ src/Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div className='table-container'>
+        <table>
+            <tbody>
+                
+            </tbody>
+        </table>
+        
+    </div>
+  )
+}
+
+export default Table
